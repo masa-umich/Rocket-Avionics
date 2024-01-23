690ec1d311625639c6681fad4146922c02cfbac2
jhammerberg <jackmh@umich.edu>
2024-01-23 01:07:41 -0500

    Temperature function test - not sure if the conversion is right

--- drivers/LSM6DSO32XTR/inc/LSM6DSO32XTR.h
+++ drivers/LSM6DSO32XTR/inc/LSM6DSO32XTR.h
@@ -10,15 +10,18 @@
 
 #define WHO_AM_I_REG_ADDR (uint8_t) 0x0F //Who am I register address
 #define WHO_AM_I_REG_VAL (uint8_t) 0x6C //Who am I register value
+#define OUT_TEMP_L (uint8_t) 0x20 //Temperature output register address
+#define OUT_TEMP_H (uint8_t) 0x21 //Temperature output register address
 
-#include <stm32h723xx.h>
+#include <stm32h723xx.h> //I added this for intellisense to work, this might not be needed
 #include "stm32h7xx_hal.h"
 #include "FreeRTOS.h"
 #include "task.h"
 
 typedef struct {
     SPI_HandleTypeDef* hspi;
-    
+    uint16_t SPI_TIMEOUT;
+
     GPIO_TypeDef * CS_GPIO_Port;
     uint16_t CS_GPIO_Pin;
 } IMU;
@@ -37,6 +40,9 @@ typedef struct {
     float ang_rate_z;
 } AngRate;
 
+//Temp convert
+float IMUfloatConvert(uint8_t H_Byte, uint8_t L_Byte)
+
 //Chip select
 void IMUchipSelect(IMU* IMU);
 
@@ -44,13 +50,13 @@ void IMUchipSelect(IMU* IMU);
 void IMUchipRelease(IMU* IMU);
 
 //Read register from IMU
-HAL_StatusTypeDef IMUread(IMU* IMU, uint8_t reg_addr, uint8_t rx_buffer, uint8_t num_bytes, uint8_t timeout);
+HAL_StatusTypeDef IMUread(IMU* IMU, uint8_t reg_addr, uint8_t rx_buffer, uint8_t num_bytes);
 
 //Write register from IMU
-HAL_StatusTypeDef IMUwrite(IMU* IMU, uint8_t tx_buffer, uint8_t num_bytes, uint8_t timeout);
+HAL_StatusTypeDef IMUwrite(IMU* IMU, uint8_t tx_buffer, uint8_t num_bytes);
 
 //Initialize IMU
-int IMUinit(SPI_HandleTypeDef* hspi, IMU* IMU, GPIO_TypeDef * CS_GPIO_Port, uint16_t CS_GPIO_Pin);
+int IMUinit(SPI_HandleTypeDef* hspi, IMU* IMU, GPIO_TypeDef * CS_GPIO_Port, uint16_t CS_GPIO_Pin, uint16_t SPI_TIMEOUT);
 
 //Get acceleration from IMU
 int IMUgetAccel(IMU* IMU, Accel* accel);
--- drivers/LSM6DSO32XTR/src/LSM6DSO32XTR.c
+++ drivers/LSM6DSO32XTR/src/LSM6DSO32XTR.c
@@ -7,6 +7,18 @@
 
 #include "../inc/LSM6DSO32XTR.h"
 
+//Temp convert
+float IMUfloatConvert(uint8_t H_Byte, uint8_t L_Byte) {
+    int16_t temp_raw = (H_Byte << 8) | L_Byte; // Combine the two bytes into one 16 bit number
+    if (temp_raw & 0x8000) { // If the sign bit is set
+        temp_raw = ~temp_raw + 1; // Calculate two's complement
+        temp_raw = -temp_raw; // Convert to negative
+    }
+    float scale_factor = 0.00190734863; // 125/2^16 (operating range divided by ADC resolution)
+    float temp = temp_raw * scale_factor; // Convert to degrees C
+    return temp;
+}
+
 //Chip select
 void IMUchipSelect(IMU* IMU) {
 	HAL_GPIO_WritePin(IMU->CS_GPIO_Port, IMU->CS_GPIO_Pin, GPIO_PIN_RESET); //CS pin
@@ -18,7 +30,7 @@ void IMUchipRelease(IMU* IMU) {
 }
 
 //Read register from IMU
-HAL_StatusTypeDef IMUread(IMU* IMU, uint8_t reg_addr, uint8_t rx_buffer, uint8_t num_bytes, uint8_t timeout) {
+HAL_StatusTypeDef IMUread(IMU* IMU, uint8_t reg_addr, uint8_t rx_buffer, uint8_t num_bytes) {
 	uint8_t reg_buffer[0] = {reg_addr | 0x80};  // Set register for reading
 
 	HAL_StatusTypeDef status;
@@ -26,8 +38,8 @@ HAL_StatusTypeDef IMUread(IMU* IMU, uint8_t reg_addr, uint8_t rx_buffer, uint8_t
 	taskENTER_CRITICAL();
 
 	IMUchipSelect(IMU);
-	HAL_SPI_Transmit(IMU->hspi, (uint8_t *)reg_buffer, 1, timeout);
-	status = HAL_SPI_Receive(IMU->hspi, (uint8_t *)rx_buffer, num_bytes, timeout);
+	HAL_SPI_Transmit(IMU->hspi, (uint8_t *)reg_buffer, 1, IMU->SPI_TIMEOUT);
+	status = HAL_SPI_Receive(IMU->hspi, (uint8_t *)rx_buffer, num_bytes, IMU->SPI_TIMEOUT);
 	IMUchipRelease(IMU);
 
 	taskEXIT_CRITICAL();
@@ -35,13 +47,13 @@ HAL_StatusTypeDef IMUread(IMU* IMU, uint8_t reg_addr, uint8_t rx_buffer, uint8_t
 }
 
 //Write register from IMU
-HAL_StatusTypeDef IMUwrite(IMU* IMU, uint8_t tx_buffer, uint8_t num_bytes, uint8_t timeout) {
+HAL_StatusTypeDef IMUwrite(IMU* IMU, uint8_t tx_buffer, uint8_t num_bytes) {
 	HAL_StatusTypeDef status;
 
 	taskENTER_CRITICAL();
 
 	IMUchipSelect(IMU);
-	status = HAL_SPI_Transmit(IMU->hspi, (uint8_t *)tx_buffer, num_bytes + 1, timeout);
+	status = HAL_SPI_Transmit(IMU->hspi, (uint8_t *)tx_buffer, num_bytes + 1, IMU->SPI_TIMEOUT);
 	IMUchipRelease(IMU);
 
 	taskEXIT_CRITICAL();
@@ -49,14 +61,15 @@ HAL_StatusTypeDef IMUwrite(IMU* IMU, uint8_t tx_buffer, uint8_t num_bytes, uint8
 }
 
 //Initialize IMU
-int IMUinit(SPI_HandleTypeDef* hspi, IMU* IMU, GPIO_TypeDef * CS_GPIO_Port, uint16_t CS_GPIO_Pin) {
+int IMUinit(SPI_HandleTypeDef* hspi, IMU* IMU, GPIO_TypeDef * CS_GPIO_Port, uint16_t CS_GPIO_Pin, uint16_t SPI_TIMEOUT) {
 	IMU->hspi = hspi;
 	IMU->CS_GPIO_Port = CS_GPIO_Port;
 	IMU->CS_GPIO_Pin = CS_GPIO_Pin;
+	IMU->SPI_TIMEOUT = SPI_TIMEOUT;
 
 	//Read WHO_AM_I register
 	uint8_t rx_buffer;
-	IMUread(IMU, WHO_AM_I_REG_ADDR, rx_buffer, 1, 1000);
+	IMUread(IMU, WHO_AM_I_REG_ADDR, rx_buffer, 1);
 	//Check if WHO_AM_I register is correct
 	if (rx_buffer != WHO_AM_I_REG_VAL) {
 		return -1;
@@ -78,6 +91,11 @@ int IMUgetAngRate(IMU* IMU, AngRate* AngRate) {
 
 //Get temperature from IMU
 int IMUgetTemp(IMU* IMU, float* temp) {
+	uint8_t buf[2];
+
+	IMUread(IMU, OUT_TEMP_L, buf, 2);
+	*temp = IMUfloatConvert(buf[1], buf[0]);
+
 	return 0;
 }
 
