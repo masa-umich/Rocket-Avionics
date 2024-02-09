3bbce9cfc7cf2c787a32e981f768b04f69d442df
jhammerberg <jackmh@umich.edu>
2024-02-09 18:02:20 -0500

    weird register behavior again yaaaay

--- drivers/LPS22HBTR/inc/LPS22HBTR.h
+++ drivers/LPS22HBTR/inc/LPS22HBTR.h
@@ -9,6 +9,7 @@
 #define LPS22HBTR_H
 
 #include "main.h"
+#include <math.h>
 
 #ifndef FreeRTOS_H
     #include "FreeRTOS.h"
@@ -18,11 +19,26 @@
 #endif
 
 // Begin register map
-#define BAR_WHO_AM_I_ADDR		(uint8_t)0x0F
-
-// todo: add register map
+#define BAR_WHO_AM_I_REG_ADDR		    (uint8_t)0x0F
+#define BAR_CTRL1_C                     (uint8_t)0x12
+#define BAR_CTRL2_C                     (uint8_t)0x13
+#define BAR_CTRL3_C                     (uint8_t)0x14
+#define BAR_PRESS_OUT_XL                (uint8_t)0x28
+#define BAR_PRESS_OUT_L                 (uint8_t)0x29
+#define BAR_PRESS_OUT_H                 (uint8_t)0x2A
+#define BAR_TEMP_OUT_L                  (uint8_t)0x2B
+#define BAR_TEMP_OUT_H					(uint8_t)0x2C
+// todo: the rest of these
 
 // End register map
+
+// Constants
+#define BAR_WHO_AM_I_REG_VAL            (uint8_t)0xB1
+#define BAR_DEFAULT_CONF_CTRL1_C        (uint8_t)0xA0 //10100000 75hz with sequential register update
+#define BAR_DEFAULT_CONF_CTRL2_C        (uint8_t)0x00 //00010000 enable sequential register update
+#define BAR_DEFAULT_CONF_CTRL3_C        (uint8_t)0x00 //00000000
+#define BAR_PRES_SCALING_FACTOR         (uint16_t)4096 //Given in datasheet as 4096 LSB/hPa
+#define BAR_TEMP_SCALING_FACTOR         (uint16_t)100 //Given in datasheet as 100 LSB/degC (section 3.1)
 
 typedef struct {
     //SPI stuff
@@ -37,10 +53,13 @@ typedef struct {
 } BAR;
 
 //Pressure convert
-float BAR_presConvert(uint8_t H_Byte, uint8_t L_Byte);
+float BAR_presConvert(uint8_t XL_Byte, uint8_t H_Byte, uint8_t L_Byte);
 
 //Altitude convert
-float BAR_altConvert(uint8_t H_Byte, uint8_t L_Byte);
+float BAR_altConvert(uint8_t XL_Byte, uint8_t H_Byte, uint8_t L_Byte, float BAR_SEA_LEVEL_PRESS);
+
+//Temperature convert
+float BAR_tempConvert(uint8_t L_Byte, uint8_t H_Byte);
 
 //Chip select
 void BAR_chipSelect(BAR* BAR);
@@ -54,14 +73,17 @@ HAL_StatusTypeDef BAR_read(BAR* BAR, uint8_t reg_addr, uint8_t* rx_buffer, uint8
 //Write register from barometer
 HAL_StatusTypeDef BAR_write(BAR* BAR, uint8_t* tx_buffer, uint8_t num_bytes);
 
-//Initialize BAR
+//Initialize barometer
 int BAR_init(BAR* BAR);
 
-//Get pressure from BAR
+//Get pressure from barometer
 int BAR_getPres(BAR* BAR, float* pres);
 
-//Get angular rate from BAR
-int BAR_getAlt(BAR* BAR, float* alt);
+//Get angular rate from barometer
+int BAR_getAlt(BAR* BAR, float* alt, float BAR_SEA_LEVEL_PRESS);
+
+//Get temperature from barometer
+int BAR_getTemp(BAR* BAR, float* temp);
 
 //Send command to barometer
 int BAR_send(BAR* BAR, uint8_t cmd, uint8_t value);
