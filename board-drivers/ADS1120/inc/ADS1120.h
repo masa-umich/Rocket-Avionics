7aa7ebfeedd2f1ff343715e916ecf50cc93e1800
Felix F-B <felixfbpuppy@gmail.com>
2025-01-08 20:37:20 -0500

    Thermocouple driver progress

--- board-drivers/ADS1120/inc/ADS1120.h
+++ board-drivers/ADS1120/inc/ADS1120.h
@@ -9,6 +9,7 @@
 #define ADS1120_H
 
 #include "main.h"
+#include <math.h>
 
 #ifndef FreeRTOS_H
     #include "FreeRTOS.h"
@@ -18,6 +19,81 @@
 #endif
 
 
-//CODE
+// Constants
+//Hard-Coded
+#define ADS_PGA_ENABLED            		(uint8_t)0x00 // Default
+#define ADS_MODE_NORMAL            		(uint8_t)0x00 // Default
+#define ADS_CONV_MODE_CONT            	(uint8_t)0x04 // Default
+#define ADS_INTERNAL_TEMP_DISABLED      (uint8_t)0x00 // Default
+#define ADS_BURN_OUT_DISABLED           (uint8_t)0x00 // Default
+#define ADS_VOLT_REF_EXT_REF0           (uint8_t)0x40 // Default
+#define ADS_FILTER_DISABLED           	(uint8_t)0x00 // Default
+#define ADS_PSW_DISABLED            	(uint8_t)0x00 // Default
+#define ADS_IDAC_DISABLED            	(uint8_t)0x00 // Default
+#define ADS_l1MUX_DISABLED            	(uint8_t)0x00 // Default
+#define ADS_l2MUX_DISABLED            	(uint8_t)0x00 // Default
+#define ADS_DRDY_MODE_ONLY_DRDY         (uint8_t)0x00 // Default
+
+//Open
+#define ADS_MUX_AIN0_AIN1            	(uint8_t)0x00 // AIN0 is positive, AIN1 is negative
+#define ADS_MUX_AIN0_AIN2            	(uint8_t)0x10 // AIN0 is positive, AIN2 is negative
+#define ADS_MUX_AIN0_AIN3            	(uint8_t)0x20 // AIN0 is positive, AIN3 is negative
+#define ADS_MUX_AIN1_AIN2            	(uint8_t)0x30 // AIN1 is positive, AIN2 is negative
+#define ADS_MUX_AIN1_AIN3            	(uint8_t)0x40 // AIN1 is positive, AIN3 is negative
+#define ADS_MUX_AIN2_AIN3            	(uint8_t)0x50 // AIN2 is positive, AIN3 is negative
+#define ADS_MUX_AIN1_AIN0            	(uint8_t)0x60 // AIN1 is positive, AIN0 is negative
+#define ADS_MUX_AIN3_AIN2            	(uint8_t)0x70 // AIN3 is positive, AIN2 is negative
+#define ADS_PGA_GAIN_1            		(uint8_t)0x00 // 1x Gain
+#define ADS_PGA_GAIN_2            		(uint8_t)0x02 // 2x Gain
+#define ADS_PGA_GAIN_4            		(uint8_t)0x04 // 4x Gain
+#define ADS_PGA_GAIN_8            		(uint8_t)0x06 // 8x Gain
+#define ADS_PGA_GAIN_16            		(uint8_t)0x08 // 16x Gain
+#define ADS_PGA_GAIN_32            		(uint8_t)0x0A // 32x Gain
+#define ADS_PGA_GAIN_64            		(uint8_t)0x0C // 64x Gain
+#define ADS_PGA_GAIN_128            	(uint8_t)0x0E // 128x Gain
+#define ADS_DATA_RATE_20            	(uint8_t)0x00 // 20 SPS ~ 20hz in normal mode
+#define ADS_DATA_RATE_45            	(uint8_t)0x20 // 45 SPS ~ 45hz in normal mode
+#define ADS_DATA_RATE_90            	(uint8_t)0x40 // 90 SPS ~ 90hz in normal mode
+#define ADS_DATA_RATE_175            	(uint8_t)0x60 // 175 SPS ~ 175hz in normal mode
+#define ADS_DATA_RATE_330            	(uint8_t)0x80 // 330 SPS ~ 330hz in normal mode
+#define ADS_DATA_RATE_600            	(uint8_t)0xA0 // 600 SPS ~ 600hz in normal mode
+#define ADS_DATA_RATE_1000            	(uint8_t)0xC0 // 1000 SPS ~ 1000hz in normal mode
+
+
+typedef struct {
+    SPI_HandleTypeDef* hspi;
+    uint16_t SPI_TIMEOUT;
+    GPIO_TypeDef * CS_GPIO_Port;
+    uint16_t CS_GPIO_Pin;
+
+    uint8_t gain;
+    double cold_junction_voltage; // microvolts
+    double voltage_ref; // microvolts
+} ADS1120;
+
+
+double ADS_convertRawToMicrovolts(ADS1120 *ADS, uint8_t MSB, uint8_t LSB); //DONE
+
+float ADS_polyMicrovoltsToTemp(double microvolts);
+
+double ADS_polyTempToMicrovolts(float temp);
+
+// Reset chip, wait small delay maybe?, configure chip, save cjc and ref voltage, start conversion
+// THIS FUNCTION IS BLOCKING, IT DELAYS FOR AT LEAST 1 MILLISECOND TO ENSURE THE CHIP HAS RESET
+// reference_volts is in volts, cold_junction_temp is in celsius
+int ADS_init(ADS1120 *ADS, uint8_t mux, uint8_t gain, uint8_t rate, double reference_volts, float cold_junction_temp);
+
+HAL_StatusTypeDef ADS_write(ADS1120 *ADS, uint8_t *tx_buffer, uint8_t num_bytes); //DONE
+
+HAL_StatusTypeDef ADS_read(ADS1120 *ADS, uint8_t reg_addr, uint8_t *rx_buffer, uint8_t num_bytes); //DONE
+
+void ADS_chipSelect(ADS1120 *ADS); //DONE
+
+void ADS_chipRelease(ADS1120 *ADS); //DONE
+
+int ADS_configure(ADS1120 *ADS, uint8_t mux, uint8_t gain, uint8_t rate, int check); //DONE
+
+// Read RDATA, convert to microvolts, offset cjc voltage, convert to temp
+int ADS_readTemp(ADS1120 *ADS, float *temp);
 
 #endif
--- board-drivers/ADS1120/src/ADS1120.c
+++ board-drivers/ADS1120/src/ADS1120.c
@@ -7,3 +7,82 @@
 
 #include "ADS1120.h"
 
+
+double ADS_convertToMicrovolts(ADS1120 *ADS, uint8_t MSB, uint8_t LSB) {
+	int16_t raw_value = (MSB << 8) | LSB;
+	double stepSize = ADS->voltage_ref / pow(2, 15 + (ADS->gain >> 1));
+	return stepSize * raw_value;
+}
+
+int ADS_init(ADS1120 *ADS, uint8_t mux, uint8_t gain, uint8_t rate, double reference_volts, float cold_junction_temp) {
+	uint8_t resetcmd[1] = {0x06};
+	ADS_write(ADS, resetcmd, 1);
+	osDelay(1);
+
+	ADS->voltage_ref = reference_volts * 1e6;
+	// convert cjc temp to voltage using t type thermocouple polynomial //TODO
+
+	int confStatus = ADS_configure(ADS, mux, gain, rate, 1);
+
+	uint8_t startcmd[1] = {0x08};
+	ADS_write(ADS, startcmd, 1);
+
+	return confStatus;
+}
+
+HAL_StatusTypeDef ADS_write(ADS1120 *ADS, uint8_t *tx_buffer, uint8_t num_bytes) {
+	taskENTER_CRITICAL();
+
+	ADS_chipSelect(ADS);
+	HAL_StatusTypeDef status = HAL_SPI_Transmit(ADS->hspi, txbuffer, num_bytes, ADS->SPI_TIMEOUT);
+	ADS_chipRelease(ADS);
+
+	taskEXIT_CRITICAL();
+
+	return status;
+}
+
+HAL_StatusTypeDef ADS_read(ADS1120 *ADS, uint8_t reg_addr, uint8_t *rx_buffer, uint8_t num_bytes) {
+	taskENTER_CRITICAL();
+
+	ADS_chipSelect(ADS);
+	HAL_SPI_Transmit(ADS->hspi, &reg_addr, 1, ADS->SPI_TIMEOUT);
+	HAL_StatusTypeDef status = HAL_SPI_Receive(ADS->hspi, rx_buffer, num_bytes, ADS->SPI_TIMEOUT);
+	ADS_chipRelease(ADS);
+
+	taskEXIT_CRITICAL();
+
+	return status;
+}
+
+void ADS_chipSelect(ADS1120 *ADS) {
+	HAL_GPIO_WritePin(ADS->CS_GPIO_Port, ADS->CS_GPIO_Pin, 0);
+}
+
+void ADS_chipRelease(ADS1120 *ADS) {
+	HAL_GPIO_WritePin(ADS->CS_GPIO_Port, ADS->CS_GPIO_Pin, 1);
+}
+
+int ADS_configure(ADS1120 *ADS, uint8_t mux, uint8_t gain, uint8_t rate, int check) {
+	uint8_t conf_byte_1 = mux | gain | ADS_PGA_ENABLED;
+	uint8_t conf_byte_2 = rate | ADS_MODE_NORMAL | ADS_CONV_MODE_CONT | ADS_INTERNAL_TEMP_DISABLED | ADS_BURN_OUT_DISABLED;
+	uint8_t conf_byte_3 = ADS_VOLT_REF_EXT_REF0 | ADS_FILTER_DISABLED | ADS_PSW_DISABLED | ADS_IDAC_DISABLED;
+	uint8_t conf_byte_4 = ADS_l1MUX_DISABLED | ADS_l2MUX_DISABLED | ADS_DRDY_MODE_ONLY_DRDY;
+
+	uint8_t txbuffer[5] = {0x43, conf_byte_1, conf_byte_2, conf_byte_3, conf_byte_4};
+
+	ADS_write(ADS, txbuffer, 5);
+
+	ADS->gain = gain;
+
+	if(check) {
+		uint8_t rxbuffer[4] = {0, 0, 0, 0};
+
+		ADS_read(ADS, 0x23, rxbuffer, 4);
+
+		return !(rxbuffer[0] == conf_byte_1 && rxbuffer[1] == conf_byte_2 && rxbuffer[2] == conf_byte_3 && rxbuffer[3] == conf_byte_4);
+	}
+	else {
+		return 0;
+	}
+}
