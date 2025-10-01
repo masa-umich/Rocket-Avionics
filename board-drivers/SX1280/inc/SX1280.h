b851095422ae412f148ccacaf1c8003076ba8f12
Thomas Tran <144975513+tommytt427@users.noreply.github.com>
2025-10-01 18:09:35 -0400

    Additions to interface, working on development of functions
    This is a slight overhaul and development of the interface, as well as implementing important functions.

--- board-drivers/SX1280/inc/SX1280.h
+++ board-drivers/SX1280/inc/SX1280.h
@@ -1,137 +1,258 @@
 /**
- * @file SX1280.h
- * @brief Header file for the SX1280 radio transceiver driver.
- *
- * This file contains the function prototypes for the high-level API
- * to interact with the SX1280 chip
- *
- * MASA - University of Michigan
- */
+* @file SX1280.h
+* @brief Header file for the SX1280 radio transceiver driver.
+*
+* This file contains the function prototypes for the high-level API
+* to interact with the SX1280 chip
+*
+* MASA - University of Michigan
+*/
+
 
 #include <stdint.h>
 #include "stm32h7xx_hal.h"
 
+
 #ifndef SX1280_H_
 #define SX1280_H_
 
+
 // SX1280 command opcodes, etc can be defined here or in the .c file
 
+
 //from SX1280 datasheet table 12-1
-#define SX1280_CMD_GET_STATUS               0xC0
-#define SX1280_CMD_WRITE_REGISTER           0x18
-#define SX1280_CMD_READ_REGISTER            0x19
-#define SX1280_CMD_WRITE_BUFFER             0x1A
-#define SX1280_CMD_READ_BUFFER              0x1B
-#define SX1280_CMD_SET_SLEEP                0x84
-#define SX1280_CMD_SET_STANDBY              0x80
-#define SX1280_CMD_SET_FS                   0xC1
-#define SX1280_CMD_SET_TX                   0x83
-#define SX1280_CMD_SET_RX                   0x82
-#define SX1280_CMD_SET_PACKET_TYPE          0x8A
-#define SX1280_CMD_SET_RF_FREQUENCY         0x86
-#define SX1280_CMD_SET_TX_PARAMS            0x8E
-#define SX1280_CMD_SET_MODULATION_PARAMS    0x8B
-#define SX1280_CMD_SET_PACKET_PARAMS        0x8C
-#define SX1280_CMD_SET_BUFFER_BASE_ADDRESS  0x8F
+#define SX1280_CMD_GET_STATUS 0xC0
+#define SX1280_CMD_WRITE_REGISTER 0x18
+#define SX1280_CMD_READ_REGISTER 0x19
+#define SX1280_CMD_WRITE_BUFFER 0x1A
+#define SX1280_CMD_READ_BUFFER 0x1B
+#define SX1280_CMD_SET_SLEEP 0x84
+#define SX1280_CMD_SET_STANDBY 0x80
+#define SX1280_CMD_SET_FS 0xC1
+#define SX1280_CMD_SET_TX 0x83
+#define SX1280_CMD_SET_RX 0x82
+#define SX1280_CMD_SET_PACKET_TYPE 0x8A
+#define SX1280_CMD_SET_RF_FREQUENCY 0x86
+#define SX1280_CMD_SET_TX_PARAMS 0x8E
+#define SX1280_CMD_SET_MODULATION_PARAMS 0x8B
+#define SX1280_CMD_SET_PACKET_PARAMS 0x8C
+#define SX1280_CMD_SET_BUFFER_BASE_ADDRESS 0x8F
+#define SX1280_CMD_GET_RX_BUFFER_STATUS 0x17
+
 
 /**
- * @brief status enum for driver functions
- */
+* @brief status enum for driver functions
+*/
 typedef enum {
-    SX1280_OK = 0,
-    SX1280_ERROR,
-    SX1280_BUSY,
-    SX1280_TIMEOUT
+   SX1280_OK = 0,
+   SX1280_ERROR,
+   SX1280_BUSY,
+   SX1280_TIMEOUT
 } SX1280_Status_t;
 
+
 /**
- * @brief structure to handle all necessary hardware handles/pins for the SX1280
- * instance of this struct should be passed to Initialize function
+* @brief structure to handle all necessary hardware handles/pins for the SX1280
+* instance of this struct should be passed to Initialize function
+*/
+
+
+typedef struct {
+   SPI_HandleTypeDef* spiHandle;
+   GPIO_TypeDef* nssPort;
+   uint16_t nssPin;
+   GPIO_TypeDef* resetPort;
+   uint16_t resetPin;
+   GPIO_TypeDef* busyPort;
+   uint16_t busyPin;
+   GPIO_TypeDef* irq_port; // DIO1 pin
+   uint16_t irq_pin;
+} SX1280_Hal_t;
+
+
+/**
+ * @brief packet types supported by sx1280 (Table 11-42)
+ *
  */
 
- typedef struct {
-    SPI_HandleTypeDef* spiHandle;
-    GPIO_TypeDef* nssPort;
-    uint16_t nssPin;
-    GPIO_TypeDef* resetPort;
-    uint16_t resetPin;
-    GPIO_TypeDef* busyPort;
-    uint16_t busyPin;
-    GPIO_TypeDef* irq_port; // DIO1 pin
-    uint16_t irq_pin;
- } SX1280_Hal_t;
 
- // PUBLIC API FUNCTION PROTOTYPES
+ typedef enum {
+   PACKET_TYPE_GFSK = 0x00,
+   PACKET_TYPE_LORA = 0x01,
+   PACKET_TYPE_RANGING = 0x02,
+   PACKET_TYPE_FLRC = 0x03,
+   PACKET_TYPE_BLE = 0x04
+ } SX1280_PacketType_t;
+
+
+
 
  /**
-  * @brief initializes the SX1280 radio module with the provided hardware configuration
-  * @param hal_config Pointer to a structure containing the hardware configuration for the SX1280
-  * @return status code indicating success or failure of the initialization process
-  * 
-  * additional info, func should perform basic hardware setup, reset it, check for comms and put into
-  * known state (STDBY_RC)
+  * @brief LoRa spreading factor (table 14-47)
   */
-SX1280_Status_t SX1280_Init(const SX1280_Hal_t* hal_config);
+ typedef enum {
+   LORA_SF5 = 0x50,
+   LORA_SF6 = 0x60,
+   LORA_SF7 = 0x70,
+   LORA_SF8 = 0x80,
+   LORA_SF9 = 0x90,
+   LORA_SF10 = 0xA0,
+   LORA_SF11 = 0xB0,
+   LORA_SF12 = 0xC0
+ } SX1280_LoRa_SF_t;
+
+
+ /**
+  * @brief LoRa bandwidth settings (table 14-48)
+  */
+ typedef enum {
+   LORA_BW_1600 = 0x0A,  // 1625 kHz
+   LORA_BW_800 = 0x18, // 812.5 kHz
+   LORA_BW_400 = 0x26, // 406.25 kHz
+   LORA_BW_200 = 0x34, // 203.125
+ } SX1280_LoRa_BW_t;
+
+
+ /**
+  * @brief LoRa coding rate settings (table 14-49)
+  */
+
+
+ typedef enum {
+   LORA_CR_4_5 = 0x01,
+   LORA_CR_4_6 = 0x02,
+   LORA_CR_4_7 = 0x03,
+   LORA_CR_4_8 = 0x04,
+   LORA_CR_LI_4_5 = 0x05, // low data rate optimization enabled
+   LORA_CR_LI_4_6 = 0x06,
+   LORA_CR_LI_4_8 = 0x07
+  } SX1280_LoRa_CR_t;
+
+
+ /**
+  * @brief LoRa packet header types (table 14-51)
+  */
+ typedef enum {
+   LORA_EXPLICIT_HEADER = 0x00,
+   LORA_IMPLICIT_HEADER = 0x80
+ } SX1280_LoRa_Header_Type_t;
+
+
+ /**
+  * @brief LoRa crc settings (table 14-53)
+  */
+ typedef enum {
+   LORA_CRC_OFF = 0x00,
+   LORA_CRC_ON = 0x20
+ } SX1280_LoRa_CRC_t;
+
+
+ /**
+  * @brief LoRa IQ setting (table 14-54)
+  */
+ typedef enum {
+   LORA_IQ_STANDARD = 0x40,
+   LORA_IQ_INVERTED = 0x00
+ } SX1280_LoRa_IQ_t;
+
+
+ // PUBLIC API FUNCTION PROTOTYPES ------------------//
+
 
 /**
- * @brief transmit a data payload over the radio
- * 
- * this function writes a data buffer to radio's FIFO and initiates transmission
- * note this is a blocking call and will wait until transmission is complete
- * 
- * @param data pointer to the data buffer to be transmitted
- * @param length the number of bytes to transfer
- * @return SX1280_Status_t status of the transmission
+ * @brief initialize SX1280 radio transceiver
+ *
+ * Function performs basic hardware setup for the radio, resets it,
+ * checks for comms, and puts into STDBY_RC mode
+ * must be called before any other functions
+ *
+ * @param hal_config pointer to struct containing hardware config
+ * (SPI Handle, GPIO ports, pins, etc)
+ * @return SX1280_Status_t status of initialization
  */
-
+SX1280_Status_t SX1280_Init(SX1280_Hal_t* hal_config);
 SX1280_Status_t SX1280_WriteBuffer(uint8_t* data, uint8_t length);
-
-/**
- * @brief reads received data payload from radio.
- * 
- * func checks for received packet, reads it form radio's
- * internal FIFO into buffer, returns number of bytes read
- * 
- * @param data pointer to buffer to store received data
- * @param maxLength maximum number of bytes to read into buffer
- * @return int16_t number of bytes read, or negative value on error
- */
-
 int16_t SX1280_ReadBuffer(uint8_t* data, uint8_t maxLength);
 
-// low level API prototypes
+
+// config prototypes
+/**
+* @brief set radio packet type (ex. LORA, GFSK).
+* Really we are only using LORA(?)
+*/
+void SX1280_Status_t SX1280_SetPacketType(SX1280_PacketType_t packetType);
+
 
 /**
- * @brief send a command to sx1280
- * 
- * @param opcode command opcode to send
- * @param buffer ptr to command parameters to send
- * @param size number of bytes in buffer
- * 
- */
+* @brief set radio carrier freq
+* @param frequency the frequency in Hz (ex. 2400000000 for 2.4 GHz)
+*/
+void SX1280_Status_t SX1280_SetRfFrequency(uint32_t frequency);
+
+
+/**
+* @brief set modulation param for current packet type
+* @param sf spreading factor (LoRa only)
+* @param bw bandwidth (LoRa only)
+* @param cr coding rate (LoRa only)
+*/
+void SX1280_Status_t SX1280_SetModulationParams(SX1280_LoRa_SF_t sf, SX1280_LoRa_BW_t bw, SX1280_LoRa_CR_t cr);
+
+
+/**
+* @brief set packet params for current packet type
+* @param preambleLength length of preamble in symbols
+* @param headerType explicit or implicit header (LoRa only)
+* @param payloadLength length of payload in bytes
+* @param crcOn crc on or off (LoRa only)
+* @param invertIQ standard or inverted IQ (LoRa only)
+*/
+void SX1280_Status_t SX1280_SetPacketParams(uint16_t preambleLength, SX1280_LoRa_Header_Type_t headerType, uint8_t payloadLength, SX1280_LoRa_CRC_t crc, SX1280_LoRa_IQ_t invertIQ);
+
+
+/**
+* @brief set base address for TX and RX buffers
+* @param txBaseAddress start address for the TX buffer (0-255)
+* @param rxBaseAddress start address for the RX buffer (0-255)
+*/
+void SX1280_Status_t SX1280_SetBufferBaseAddress(uint8_t txBaseAddress, uint8_t rxBaseAddress);
+
+
+/**
+* @brief sets transmit output power and ramp time
+* @param power output power in dBm (-18 to +13 dBm)
+* @param rampTime Ramp time constant.
+*/
+void SX1280_Status_t SX1280_SetTxParams(int8_t power, uint8_t rampTime);
+
+
+
+
+
+
+
+
+
+
+// low level API prototypes --------------------//
+/**
+* @brief sends a command to sx1280
+*
+* @param opcode command opcode
+* @param buffer pointer to command arguments
+* @param size number of param bytes
+*/
 void SX1280_SendCommand(uint8_t opcode, uint8_t* buffer, uint16_t size);
 
-/**
- * @brief writes data to one or more registers
- * @param address starting register address to write to
- * @param buffer pointer to data to write
- * @param size number of bytes to write
- */
-void SX1280_WriteRegister(uint16_t address, uint8_t* buffer, uint16_t size);
 
-/**
- * @brief reads data from one or more registers
- * @param address starting register address to read from
- * @param buffer pointer to buffer to store read data
- * @param size number of bytes to read
- * 
- */
+void SX1280_WriteRegister(uint16_t address, uint8_t* buffer, uint16_t size);
 void SX1280_ReadRegister(uint16_t address, uint8_t* buffer, uint16_t size);
 
 
 
 
- 
 
- // this marks the end of the header file, additional function prototypes can be added above this line
+
+// this marks the end of the header file, additional function prototypes can be added above this line
 #endif // SX1280_H_
+
--- board-drivers/SX1280/src/SX1280.c
+++ board-drivers/SX1280/src/SX1280.c
@@ -1,130 +1,147 @@
 /**
- * @file SX1280.c
- * @brief Driver for the SX1280 radio transceiver
- *
- * This file implements the high-level API and low-level SPI communication
- * for the SX1280 chip, designed for an STM32 platform using HAL and FreeRTOS.
- *
- * MASA - University of Michigan
- */
+* @file SX1280.c
+* @brief Driver for the SX1280 radio transceiver
+*
+* This file implements the high-level API and low-level SPI communication
+* for the SX1280 chip, designed for an STM32 platform using HAL and FreeRTOS.
+*
+* MASA - University of Michigan
+*/
+
 
 #include "SX1280.h"
 #include "stm32h7xx_hal.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include <string.h>
 
 
-// for clarity, this section is to hold private defines -------------------//
 
+
+//**************************************************
+// * PRIVATE DEFINES
+//**************************************************
 #define SX1280_NOP 0x00
+#define XTAL_FREQ 52000000UL // 52 MHz crystal frequency for SX1280
+
+
+
 
 //standby modes for SET_STANDBY command
 #define STDBY_RC 0x00
 #define STDBY_XOSC 0x01
 
-// add SX1280 command opcodes, etc here
 
-
-//
-
-// private static variables --------------------------//
+//***********************************************************
+// * PRIVATE VARIABLES
+//***********************************************************
 // static pointer to hardware config struct
-static const SX1280_Hal_t* sx1280_hal_config;
+static const SX1280_Hal_t* sx1280_hal_config = NULL;
 
-// private function prototypes ----------------------//
 
+//***********************************************************
+// * PRIVATE FUNCTION PROTOTYPES
+//***********************************************************
 /**
- * @brief private helper func for thread-safe spi comms
- * 
- * this function handles low-level detail of SPI transact, inclding
- * NSS chip select, busy pin wait
- * wrapped in FreeRTOS critical section for thread safety
- * 
- * @param pTxData pointer to data to be transmitted
- * @param pRxData pointer to buffer for received data
- * @param size number of bytes to transfer
- */
+* @brief private helper func for thread-safe SPI comms
+*
+* This func handles low-level details of SPI transactions, including
+* Chip select, waiting for busy line
+* Wrapped in FreeRTOS critical sections to ensure atomicity in multi-threaded
+* environment, prevents context switches during SPI transactions
+*
+* @param pTxData pointer to data to transmit
+* @param pRxData pointer to buffer for received data
+* @param size number of bytes to transfer
+*/
 static void SX1280_SPI_TransmitReceive(uint8_t* pTxData, uint8_t* pRxData, uint16_t size);
-
-/**
- * @brief waits for busy pin to go low, indicating chip is ready
- * @param timeout maximum time to wait in ms
- * @return SX1280_Status_t SX1280_OK if ready, SX1280_TIMEOUT if timeout
- */
 static SX1280_Status_t SX1280_WaitForReady(uint32_t timeout);
+
 
 // public API function definitions ------------------//
 
-SX1280_Status_t SX1280_Init(const SX1280_Hal_t* hal_config) {
-    // store hal config
-    sx1280_hal_config = hal_config;
 
-    //hardware reset sequence to be done here
+SX1280_Status_t SX1280_Init(SX1280_Hal_t* hal_config) {
+   //store hardware config
+   sx1280_hal_config = hal_config;
 
-    //set radio to known state here, ex standby mode
 
-    //further configs ex. packet type, freq, etc, here
+   // perform hardware reset
+   HAL_GPIO_WritePin(sx1280_hal_config->resetPort, sx1280_hal_config->resetPin, GPIO_PIN_RESET);
+   HAL_Delay(20); //reset pulse
+   HAL_GPIO_WritePin(sx1280_hal_config->resetPort, sx1280_hal_config->resetPin, GPIO_PIN_SET);
+   HAL_DELAY(50); //wait for chip to boot
 
-    //return success for now
-    return SX1280_OK;
+
+   //set radio to state STDBY_RC
+   uint8_t standby_mode = STDBY_RC;
+   SX1280_SendCommand(SX1280_CMD_SET_STANDBY, &standby_mode, 1);
+
+
+   // default to LoRa packet type
+   SX1280_SetPacketType(PACKET_TYPE_LORA);
+   SX1280_SetRfFrequency(2400000000); //2.4 GHz
+   SX1280_SetBufferBaseAddress(0, 128);
+   SX1280_SetModulationParams(LORA_SF7, LORA_BW_1600, LORA_CR_4_5);
+   SX1280_SetPacketParams(12, LORA_EXPLICIT_HEADER, 255, LORA_CRC_ON, LORA_IQ_STANDARD);
+   SX1280_SetTxParams(0, 0x80); //0dbm, 10us ramp time
+   return SX1280_OK;
 }
 
-SX1280_Status_t SX1280_WriteBuffer(uint8_t* data, uint16_t length) {
-    // implementation to be added
-    //involves setting packet parameters writing payload to
-    //radio buffer, issuing transmit comand
-    (void)data;
-    (void)length;
 
-    return SX1280_OK;
+SX1280_Status_t SX1280_WriteBuffer(uint8_t *data, uint8_t length)
+{
+   uint8_
 }
 
-int16_t SX1280_ReadBuffer(uint8_t* data, uint8_t maxLength) {
-    // implmementation to be added
-    // involves checking IRQ status, getting received packet's
-    //length, reading payload from radio buffer
-    (void)data;
-    (void)maxLength;
-    return 0; //return number of bytes read
-}
 
-// private function definitions -------------------//
+   // private function definitions -------------------//
 
-static void SX1280_SPI_TransmitReceive(uint8_t* pTxData, uint8_t* pRxData, uint16_t size) {
-    // wait for radio to not be busy before starting SPI transaction
-    SX1280_WaitForReady(100); //100ms timeout
 
-    //enter critical section for thread safety
-    taskENTER_CRITICAL();
+   static void SX1280_SPI_TransmitReceive(uint8_t *pTxData, uint8_t *pRxData, uint16_t size)
+   {
+       // wait for radio to not be busy before starting SPI transaction
+       SX1280_WaitForReady(100); // 100ms timeout
 
-    //set NSS low to select the radio
-    HAL_GPIO_WritePin(sx1280_hal_config->nssPort, sx1280_hal_config->nssPin, GPIO_PIN_RESET);
 
-    //perform SPI transaction
-    if (pRxData == NULL)
-    {
-        HAL_SPI_Transmit(sx1280_hal_config->spiHandle, pTxData, size, HAL_MAX_DELAY);
-    }
-    else
-    {
-        HAL_SPI_TransmitReceive(sx1280_hal_config->spiHandle, pTxData, pRxData, size, HAL_MAX_DELAY);
-    }
-    //set NSS high to deselect the radio
-    HAL_GPIO_WritePin(sx1280_hal_config->nssPort, sx1280_hal_config->nssPin, GPIO_PIN_SET);
-    //exit critical section
-    taskEXIT_CRITICAL();
+       // enter critical section for thread safety
+       taskENTER_CRITICAL();
 
-    //wait for radio to be ready after transaction
-    SX1280_WaitForReady(100); //100ms timeout
 
-}
+       // set NSS low to select the radio
+       HAL_GPIO_WritePin(sx1280_hal_config->nssPort, sx1280_hal_config->nssPin, GPIO_PIN_RESET);
 
-static SX1280_Status_t SX1280_WaitForReady(uint32_t timeout) {
-    uint32_t startTick = xTaskGetTickCount();
-    while (HAL_GPIO_ReadPin(sx1280_hal_config->busyPort, sx1280_hal_config->busyPin) == GPIO_PIN_SET) {
-        if ((xTaskGetTickCount() - startTick) > timeout) {
-            return SX1280_TIMEOUT;
-        }
-    }
-    return SX1280_OK;
-}
+
+       // perform SPI transaction
+       if (pRxData == NULL)
+       {
+           HAL_SPI_Transmit(sx1280_hal_config->spiHandle, pTxData, size, HAL_MAX_DELAY);
+       }
+       else
+       {
+           HAL_SPI_TransmitReceive(sx1280_hal_config->spiHandle, pTxData, pRxData, size, HAL_MAX_DELAY);
+       }
+       // set NSS high to deselect the radio
+       HAL_GPIO_WritePin(sx1280_hal_config->nssPort, sx1280_hal_config->nssPin, GPIO_PIN_SET);
+       // exit critical section
+       taskEXIT_CRITICAL();
+
+
+       // wait for radio to be ready after transaction
+       SX1280_WaitForReady(100); // 100ms timeout
+   }
+
+
+   static SX1280_Status_t SX1280_WaitForReady(uint32_t timeout)
+   {
+       uint32_t startTick = xTaskGetTickCount();
+       while (HAL_GPIO_ReadPin(sx1280_hal_config->busyPort, sx1280_hal_config->busyPin) == GPIO_PIN_SET)
+       {
+           if ((xTaskGetTickCount() - startTick) > timeout)
+           {
+               return SX1280_TIMEOUT;
+           }
+       }
+       return SX1280_OK;
+   }
+
