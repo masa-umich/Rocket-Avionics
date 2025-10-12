6820c0d343021f881a4f99d86218f3ca5043d39a
Felix F-B <felixfbpuppy@gmail.com>
2025-10-12 19:14:08 -0400

    Move telemetry code

---
+++ stm-flight-sw/limestone-firmware/Core/Inc/telemetry.h
@@ -0,0 +1,13 @@
+/*
+ * telemetry.h
+ *
+ *  Created on: Oct 12, 2025
+ *      Author: felix
+ */
+
+#ifndef INC_TELEMETRY_H_
+#define INC_TELEMETRY_H_
+
+void TelemetryTask(void *argument);
+
+#endif /* INC_TELEMETRY_H_ */
