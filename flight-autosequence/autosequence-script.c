4ee7f9dc6b3faa27916b92cd75f45200c54a0b57
Soren Walther <swalther@umich.edu>
2025-10-28 15:14:45 -0400

    started writing basic structure

---
+++ flight-autosequence/autosequence-script.c
@@ -0,0 +1,8 @@
+#include "state-machine.h"
+
+const int MPV_DELAY = 100000;
+const int BURN_DURATION = 22000000;
+
+State* create_states(){
+
+}
---
+++ flight-autosequence/limestone-interface.h
@@ -0,0 +1,11 @@
+#ifndef LIMESTONE_INTERFACE_H
+#define LIMESTONE_INTERFACE_H
+
+#include <stdint.h>
+
+uint32_t getTime();
+
+uint32_t startTimer();
+
+
+#endif
