97b7122da1b0cf2cccf9d878cf955c912d23d3ab
Soren Walther <swalther@umich.edu>
2025-12-02 15:31:42 -0500

    incorporated apogee detection, need to debug

--- flight-autosequence/autosequence-script.c
+++ flight-autosequence/autosequence-script.c
@@ -3,7 +3,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include "autosequence-script.h"
-
+#include "apogee-functions.h"
+#include "main.h"
+#include "temp-header.h"
 
 #define STATE0_NAME "Open MPV1"
 #define STATE1_NAME "Open both MPVs"
@@ -12,100 +14,200 @@
 #define STATE01_TRANSITION_MESSAGE "Opening Ox MPV"
 //#define STATE01_TRANSITION_MESSAGE "Opening Ox MPV"   
 
-typedef enum {
-    STATE0 = 0,
-    STATE1,
-    STATE2,
-    STATE3,
-    STATE4,
-    STATE5
-} States;
 
 const int MPV_DELAY = 0.5 * 1000; // in milliseconds
 const int BURN_DURATION = 22 * 1000; // in milliseconds
+const int MAX_MECO_WAIT = 30 * 1000; // millis
 
-uint32_t handoff_timestamp;
-uint32_t ignition_timestamp;
-
-void set_handoffTS(){
-    ignition_timestamp = getTime();
-}
-
-void set_ignitionTS(){
-    handoff_timestamp = getTime();
-}
-
-uint32_t time_since_handoff() {
-    return getTime() - handoff_timestamp;
-}
-
-uint32_t time_since_igntion() {
-    return getTime() - ignition_timestamp;
-}
-
-bool state0_transition(){
-    if (time_since_handoff() >= MPV_DELAY){
-        set_ignitionTS();
-        return true;
-    }
-
-    return false;
-}
-
-bool state1_transition(){
-    return time_since_igntion() >= BURN_DURATION;
-}
-
-void state0_output(){
-    energizeMPV1();
-}
-
-void state1_output(){
-    energizeMPV1();
-    energizeMPV2();
-}
-
-/*
-State* create_states(){
-    State* s0;
-    state_setName(s0, STATE0_NAME);
-    state_setNumTransitions(s0);
-    state_addTransition(s0, &state0_transition);
-    state_setOutput(s0, &state0_output);
-
-
-    State* s1;
-    state_setName(s1, STATE1_NAME);
-    state_setNumTransitions(s1);
-    state_addTransition(s1, &state1_transition);
-    state_setOutput(s1, &state1_output);
-
-    return s0; // return first state;
-}
-*/
 
 void execute_flight_autosequence(){
-    uint8_t current_state = STATE0; // starts at state0
+    FlightPhase phase = ST_OPEN_MPV1; // starts at state0
+
+    const uint32_t period = pdMS_TO_TICKS(50); // 20 Hz should be good
+    uint32_t last = xTaskGetTickCount();
+
+    Detector baro_detector = {0};
+    Detector imu_detector = {0};
+    Detector temp_C_detector = {0};
+
+    // For subsonic flights just set 'phase' to ST_WAIT_APOGEE
+    // Doing so will bypass the MECO phase and immediately arm/monitor the barometers
+    // ST_WAIT_MECO is phase 1 for full supersonic flights
+    // flags
+
+    int MECO_flag = 0;
+    int apogee_flag = 0;
+    int drogue_flag = 0;
+    int main_flag = 0;
+
+    uint32_t handoff_timestamp = 0; // time (in ms) when handoff occurs
+    uint32_t ignition_timestamp = 0;// approximate time (in ms) when igntion occurs
+    uint32_t meco_timestamp = 0;         // time (in ms) when MECO is detected
+    // uint32_t lockout_tick = 0; // will be computed when MECO_flag is set --> Relevance of lockout?
 
     for (;;){
-        switch (current_state) {
-            case STATE0:
-                state0_output();
 
-                if (state0_transition()){
-                    current_state = STATE1;
-                } 
-                break;
-            
-            case STATE1:
-                state1_output();
+        float bar1 = 0.0f;
+        float bar2 = 0.0f;
+        float imu1 = 0.0f;
+        float imu2 = 0.0f;
 
-                if (state1_transition()){
-                    current_state = STATE2;
-                }
-                break;
+        //temp from barometers not yet implemented in barometer driver
+        //but would be helpful here, otherwise will need to estimate it
+        float bar1_temp_K = 0.0f;
+        float bar2_temp_K = 0.0f;
 
-            
+        if (xSemaphoreTake(Rocket_h.fcState_access, pdMS_TO_TICKS(5)) == pdPASS)
+        {
+            bar1 = Rocket_h.fcState.bar1; // convert to needed units if necessary
+            bar2 = Rocket_h.fcState.bar2;
+            imu1 = Rocket_h.fcState.imu1_A.XL_x;
+            imu2 = Rocket_h.fcState.imu2_A.XL_x;
+
+            //Temp from barometers would be useful here too 
+            //immediately convert to Kelvin
+            bar1_temp_K = Rocket_h.fcState.bar1_temp_C + 273.15f;
+            bar2_temp_K = Rocket_h.fcState.bar2_temp_C + 273.15f;
+
+            xSemaphoreGive(Rocket_h.fcState_access);
         }
+
+
+
+        switch (phase) {
+            case ST_OPEN_MPV1:
+                energizeMPV1();
+
+                if (getTime() - handoff_timestamp > MPV_DELAY){
+                    ignition_timestamp = getTime();
+                    phase = ST_OPEN_BOTH_MPVS;
+                }                    
+
+                break;
+
+            
+            case ST_OPEN_BOTH_MPVS:
+                energizeMPV1();
+                energizeMPV2();
+                
+                phase = ST_WAIT_MECO;
+                break;
+
+
+            case ST_WAIT_MECO: {
+                insert(&imu_detector, imu1, imu2, phase);
+                insert(&baro_detector, bar1, bar2, phase);
+                insert(&temp_C_detector, bar1_temp_K, bar2_temp_K, phase); // insert temp in K or C?
+
+                if(imu_detector.avg_size >= AD_CAPACITY)
+                {
+
+                    if (detect_MECO(&imu_detector) || getTime() - ignition_timestamp > MAX_MECO_WAIT)
+                    {
+                        MECO_flag = 1;
+                        meco_timestamp = xTaskGetTickCount();
+                        // TODO: SEND EVENT
+                        phase = ST_WAIT_APOGEE;
+
+                        /*
+                        //COMPUTE LOCKOUT/WAIT-TIME
+                        {
+                            float avg_pressure;
+                            float avg_temp;
+
+                            //if baro buffer is ready just get the average
+                            if(baro_detector.avg_size >= AD_CAPACITY)
+                            {
+                                int idx = (baro_detector.avg_index + AD_CAPACITY - 1) % AD_CAPACITY;
+                                avg_pressure = baro_detector.average[idx];
+                            }
+                            //emergency fallback, avg the two most recent barometer readings
+                            else
+                            {
+                                //MUST be hPa/mbar coming in from Rocket_h
+                                avg_pressure = 0.5f * ((bar1) + (bar2));
+                            }
+
+                            //if temp buffer is ready just get the average
+                            if (temp_C_detector.avg_size >= AD_CAPACITY)
+                            {
+                                int idx = (temp_C_detector.avg_index + AD_CAPACITY - 1) % AD_CAPACITY;
+                                avg_temp = temp_C_detector.average[idx];
+                            }
+                            //emergency fallback, avg the two most recent temperature readings
+                            else
+                            {
+                                //MUST be Celsius coming in from Rocket_h
+                                avg_temp = 0.5f * ((bar1_temp_C) + (bar2_temp_C));
+                            }
+
+                                int meco_time_ms = (int)(meco_tick * portTICK_PERIOD_MS);
+
+                                int wait_time_ms = compute_wait_time(meco_time_ms, avg_pressure, avg_temp);
+
+                                //lockout_tick = pdMS_TO_TICKS(wait_time_ms);
+                        }
+
+                        */
+                    }
+                }
+
+                break;
+            } 
+
+
+            case ST_WAIT_APOGEE: {
+                insert(&baro_detector, bar1, bar2, phase);
+
+                if(baro_detector.slope_size >= AD_CAPACITY) {
+                    if (detect_apogee(&baro_detector)) {
+                        apogee_flag = 1;
+                        // TODO: SEND EVENT
+                        phase = ST_WAIT_DROGUE;
+                    }
+                }
+
+                break;
+            } 
+
+            case ST_WAIT_DROGUE: {
+                insert(&baro_detector, bar1, bar2, phase);
+            
+                if(baro_detector.avg_size >= AD_CAPACITY)
+                {
+                    if (detect_altitude(&baro_detector, phase))
+                    {
+                        drogue_flag = 1;
+                        // TODO: SEND EVENT
+                        phase = ST_WAIT_MAIN;
+                    }
+                }
+
+                break;
+            } 
+
+
+            case ST_WAIT_MAIN: {
+                insert(&baro_detector, bar1, bar2, phase);
+                if(baro_detector.avg_size >= AD_CAPACITY)
+                {
+                    if (detect_altitude(&baro_detector, phase))
+                    {
+                        main_flag = 1;
+                        // TODO: SEND EVENT
+                        phase = ST_DONE;
+                    }
+                }
+
+                break;
+            } 
+
+            case ST_DONE: {
+                vTaskSuspend(NULL);
+            } break;
+            
+        }//end switch(phase)
+
+        vTaskDelayUntil(&last, period);
     }
 }
