31ad8292264c4d4ef83b284a96693856fa65411c
Soren Walther <swalther@umich.edu>
2026-02-01 15:53:13 -0500

    bug fixes

--- flight-autosequence/autosequence-script.c
+++ flight-autosequence/autosequence-script.c
@@ -57,6 +57,8 @@ void execute_flight_autosequence(){
 
         //temp from barometers not yet implemented in barometer driver
         //but would be helpful here, otherwise will need to estimate it
+        float bar1_temp_C = 0.0f;
+        float bar2_temp_C = 0.0f;
         float bar1_temp_K = 0.0f;
         float bar2_temp_K = 0.0f;
 
@@ -72,16 +74,18 @@ void execute_flight_autosequence(){
             imu1 = Rocket_h.fcState.imu1_A.XL_x;
             imu2 = Rocket_h.fcState.imu2_A.XL_x;
 
+            /*
             accel_x = (Rocket_h.fcstate.imu1.XL_x + Rocket_h.fcstate.imu2.XL_x) / 2.0f;
             accel_y = (Rocket_h.fcstate.imu1.XL_y + Rocket_h.fcstate.imu2.XL_y) / 2.0f;
             accel_z = (Rocket_h.fcstate.imu1.XL_z + Rocket_h.fcstate.imu2.XL_z) / 2.0f;
             gyro_x = (Rocket_h.fcstate.imu1.W_X + Rocket_h.fcstate.imu2.W_X) / 2.0f;
             gyro_y = (Rocket_h.fcstate.imu1.W_Y + Rocket_h.fcstate.imu2.W_Y) / 2.0f;
+            */
 
-            //Temp from barometers would be useful here too 
-            //immediately convert to Kelvin
-            bar1_temp_K = Rocket_h.fcState.bar1_temp_C + 273.15f;
-            bar2_temp_K = Rocket_h.fcState.bar2_temp_C + 273.15f;
+            bar1_temp_C = Rocket_h.fcState.bar1_temp_C;
+            bar1_temp_K = bar1_temp_C + 273.15f;
+            bar2_temp_C = Rocket_h.fcState.bar2_temp_C;
+            bar2_temp_K = bar2_temp_C + 273.15f;
 
             xSemaphoreGive(Rocket_h.fcState_access);
         }
@@ -89,7 +93,7 @@ void execute_flight_autosequence(){
 
 
         switch (phase) {
-            case ST_OPEN_MPV1: // This state currently works as intended
+            case ST_OPEN_MPV1: 
                 energizeMPV1();
 
                 if (getTime() - handoff_timestamp > MPV_DELAY){
@@ -99,69 +103,59 @@ void execute_flight_autosequence(){
 
                 break;
 
+            case ST_OPEN_BOTH_MPVS: 
+                energizeMPV1();
+                energizeMPV2();
+
+                phase = ST_WAIT_MECO;
+                 
+                break;
+
             
-            case ST_OPEN_BOTH_MPVS: // This state currently works as intended
+            case ST_WAIT_MECO: {
                 energizeMPV1();
                 energizeMPV2();
                 
-                phase = ST_WAIT_MECO;
-                break;
-
-
-            case ST_WAIT_MECO: {
                 insert(&imu_detector, imu1, imu2, phase);
                 insert(&baro_detector, bar1, bar2, phase);
-                insert(&temp_C_detector, bar1_temp_K, bar2_temp_K, phase); // insert temp in K or C?
+                insert(&temp_C_detector, bar1_temp_C, bar2_temp_C, phase); // insert temp in K or C?
 
                 if(imu_detector.avg_size >= AD_CAPACITY)
                 {
 
-                    if (detect_MECO(&imu_detector) || getTime() - ignition_timestamp > MAX_MECO_WAIT)
-                    {
+                    if (detect_MECO(&imu_detector)) {
                         MECO_flag = 1;
-                        meco_timestamp = xTaskGetTickCount();
-                        // TODO: SEND EVENT
+                        meco_timestamp = getTime();
                         phase = ST_WAIT_APOGEE;
-
                         
                         //COMPUTE LOCKOUT/WAIT-TIME
-                        {
-                            float avg_pressure;
-                            float avg_temp;
+                        float avg_pressure;
+                        float avg_temp;
 
-                            //if baro buffer is ready just get the average
-                            if(baro_detector.avg_size >= AD_CAPACITY)
-                            {
-                                int idx = (baro_detector.avg_index + AD_CAPACITY - 1) % AD_CAPACITY;
-                                avg_pressure = baro_detector.average[idx];
-                            }
-                            //emergency fallback, avg the two most recent barometer readings
-                            else
-                            {
-                                //MUST be hPa/mbar coming in from Rocket_h
-                                avg_pressure = 0.5f * ((bar1) + (bar2));
-                            }
-
-                            //if temp buffer is ready just get the average
-                            if (temp_C_detector.avg_size >= AD_CAPACITY)
-                            {
-                                int idx = (temp_C_detector.avg_index + AD_CAPACITY - 1) % AD_CAPACITY;
-                                avg_temp = temp_C_detector.average[idx];
-                            }
-                            //emergency fallback, avg the two most recent temperature readings
-                            else
-                            {
-                                //MUST be Celsius coming in from Rocket_h
-                                avg_temp = 0.5f * ((bar1_temp_K) + (bar2_temp_K));
-                            }
-
-                                int meco_time_ms = (int)(meco_timestamp * portTICK_PERIOD_MS);
-
-                                int wait_time_ms = compute_wait_time(meco_time_ms, avg_pressure, avg_temp);
-                                lockout_timestamp = pdMS_TO_TICKS(wait_time_ms);
+                        //if baro buffer is ready just get the average
+                        if(baro_detector.avg_size >= AD_CAPACITY) {
+                            int idx = (baro_detector.avg_index + AD_CAPACITY - 1) % AD_CAPACITY;
+                            avg_pressure = baro_detector.average[idx];
+                        }
+                        //emergency fallback, avg the two most recent barometer readings
+                        else {
+                            avg_pressure = 0.5f * ((bar1) + (bar2)); //MUST be hPa/mbar coming in from Rocket_h
                         }
 
-                        
+
+                        //if temp buffer is ready just get the average
+                        if (temp_C_detector.avg_size >= AD_CAPACITY) {
+                            int idx = (temp_C_detector.avg_index + AD_CAPACITY - 1) % AD_CAPACITY;
+                            avg_temp = temp_C_detector.average[idx];
+                        }
+                        //emergency fallback, avg the two most recent temperature readings
+                        else {
+                            avg_temp = 0.5f * ((bar1_temp_C) + (bar2_temp_C)); //MUST be Celsius coming in from Rocket_h
+                        }
+
+                        int meco_time_ms = (int)(meco_timestamp * portTICK_PERIOD_MS);
+                        lockout_timestamp = pdMS_TO_TICKS(compute_wait_time(meco_time_ms, avg_pressure, avg_temp)) + meco_timestamp;
+                
                     }
                 }
 
@@ -170,18 +164,21 @@ void execute_flight_autosequence(){
 
             case ST_MACH_LOCKOUT:
             {   
-                float pitch, yaw;
-                cf_update(&imu_cf, &pitch, &yaw, accel_x, accel_y, accel_z, gyro_x, gyro_y);
+                // ORIENTATION FILTER UPDATE - NOT USED YET
+                //float pitch, yaw;
+                //cf_update(&imu_cf, &pitch, &yaw, accel_x, accel_y, accel_z, gyro_x, gyro_y);
+                /*
                 for (int i = 6; i > 0; --i) {
                     pitch_buf[i] = pitch_buf[i - 1];
                     yaw_buf[i] = yaw_buf[i - 1];
                 }
                 pitch_buf[0] = pitch;
                 yaw_buf[0] = yaw;
+                */
 
                 insert(&baro_detector,bar1, bar2, phase);
 
-                if ((getTime() - meco_timestamp) >= lockout_timestamp)
+                if (getTime() >= lockout_timestamp)
                 {
                     phase = ST_WAIT_APOGEE;
                 }
