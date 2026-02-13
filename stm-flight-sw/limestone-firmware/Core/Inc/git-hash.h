815e3324092c999718e949419e0155e5acc1f1ba
Felix F-B <felixfbpuppy@gmail.com>
2026-02-13 02:12:39 -0500

    LWIP and FreeRTOS config fixed

--- stm-flight-sw/limestone-firmware/Core/Inc/git-hash.h
+++ stm-flight-sw/limestone-firmware/Core/Inc/git-hash.h
@@ -2,15 +2,15 @@
  *  git-hash.h
  *
  *  Auto-generated during the STM32 pre-build steps
- *  Last generated: Thu, 12 Feb 2026 22:59:31 -0500
+ *  Last generated: Fri, 13 Feb 2026 01:32:25 -0500
  */
 
 #ifndef INC_GIT_HASH_H_
 #define INC_GIT_HASH_H_
 
-#define GIT_HASH        "5009e68"
+#define GIT_HASH        "4131d90"
 #define GIT_BRANCH      "handoff-dev"
-#define BUILD_TIME      "Thu, 12 Feb 2026 22:59:31 -0500"
+#define BUILD_TIME      "Fri, 13 Feb 2026 01:32:25 -0500"
 #define BUILD_TYPE      "Debug"
 
 #endif /* INC_GIT_HASH_H_ */
